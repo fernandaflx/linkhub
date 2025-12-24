ead492f71f38b20062cb3f263c29ad38b34ec7b3
Fernanda Felix <fernandaleitefelix@gmail.com>
2025-12-24 01:36:51 -0300

    fix navbar links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -26,7 +26,7 @@ export function NavBar() {
   return (
     <nav className="fixed top-0 inset-x-0 z-50 flex justify-center px-4 py-3 w-screen">
       <div className="glass-panel flex w-full items-center justify-between rounded-full px-4 py-2">
-        <Link href="/" className="flex items-center gap-2">
+        <Link href={isAuthenticated ? '/dashboard' : "/"} className="flex items-center gap-2">
           <Logo />
         </Link>
 
